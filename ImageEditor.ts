948ffb0d01c928012709a07c42ee55400d723721
Merleth Munoz <merlethmunozra@gmail.com>
2026-09-15 17:51:19 -0600

    Start setup for ImageEditor Imp Color interface

--- ImageEditor.ts
+++ ImageEditor.ts
@@ -1,5 +1,39 @@
 import { readFileSync } from "node:fs";
 import process from 'process';
 
-const args: string[] = process.argv.slice(2);
-console.log(args);
+
+interface Color {
+    red : number;
+    green : number;
+    blue : number;
+}
+
+class ImageEditor {
+    public run(args: string[]): void {
+
+        try{
+            if (args.length < 3) {
+                this.usage();
+                return;
+            }
+            
+            const inputFile = args[0];
+            const outputFile = args[1];
+            const filter = args[2];
+        } catch (error) {
+            console.error("Error: " + error);
+        }
+
+    }
+
+    private usage(): void {
+        console.log("USAGE: java ImageEditor <in-file> <out-file> <grayscale|invert|emboss|motionblur> {motion-blur-length}")
+    }
+}
+
+function main(): void {
+    const args: string[] = process.argv.slice(2);
+    console.log(args);
+}
+
+main();
